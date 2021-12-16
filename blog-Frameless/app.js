e3dc64fa6a7cc6f73f7ad37d4daa0205d6d94d4b
EvenyYlzz <172463074@qq.com>
2021-12-16 22:37:00 +0800

    feat: 4-6 初始化路由

--- blog-Frameless/app.js
+++ blog-Frameless/app.js
@@ -1,18 +1,40 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req, res) => {
-  // 设置返回格式
+  // 设置返回格式 JSON
   res.setHeader('Content-type', 'application/json')
 
-  const resData = {
-    name: 'Even100',
-    site: 'juejin',
-    env: process.env.NODE_ENV,
-    // 通过该方式取得环境变量，process提供的全局变量
-    // 通过识别环境，返回不一样的数据，接口的处理等都不一致
+  // 获取path
+  const url = req.url
+  req.path = url.split('?')[0]
+
+  // 处理blog路由
+  const blogData = handleBlogRouter(req, res)
+  if (blogData) {
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return
   }
 
-  res.end(
-    JSON.stringify(resData)
-  )
+  // 处理user路由
+  const userData = handleUserRouter(req, res)
+  if (userData) {
+    res.end(
+      JSON.stringify(userData)
+    )
+    return
+  }
+
+  // 未命中路由返回404
+  res.writeHead(404, {'Content-type': 'text/plain'})
+  res.write('404 Not Found\n')
+  res.end()
 }
 
 module.exports = serverHandle
+
+// env: process.env.NODE_ENV
+// 通过该方式取得环境变量，process提供的全局变量
+// 通过识别环境，返回不一样的数据，接口的处理等都不一致
---
+++ blog-Frameless/src/router/blog.js
@@ -0,0 +1,40 @@
+const handleBlogRouter = (req, res) => {
+  const method = req.method
+
+  // 获取博客列表
+  if (method === 'GET' && req.path === '/api/blog/list') {
+    return {
+      mas: '这是获取博客列表的接口'
+    }
+  }
+
+  // 获取博客详情
+  if (method === 'GET' && req.path === '/api/blog/detail') {
+    return {
+      msg: '这是获取博客详情的接口'
+    }
+  }
+
+  // 新建博客
+  if (method === 'POST' && req.path === '/api/blog/new') {
+    return {
+      msg: '这是新建博客的接口'
+    }
+  }
+
+  // 更新博客
+  if (method === 'POST' && req.path === '/api/blog/update') {
+    return {
+      msg: '这是更新博客的接口'
+    }
+  }
+
+  // 删除博客
+  if (method === 'POST' && req.path === '/api/blog/del') {
+    return {
+      msg: '这是删除博客的接口'
+    }
+  }
+}
+
+module.exports = handleBlogRouter
