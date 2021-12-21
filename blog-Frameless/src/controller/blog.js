f521a2c7d82908083079ee674d69fc56b7a52bd9
EvenyYlzz <172463074@qq.com>
2021-12-21 23:36:12 +0800

    feat: 4-10 新建和更新博客路由

--- blog-Frameless/src/controller/blog.js
+++ blog-Frameless/src/controller/blog.js
@@ -29,7 +29,27 @@ const getDetail = (id) => {
   }
 }
 
+const newBlog = (blogData = {}) => {
+  // blogData 是一个博客对象，包含title、content属性
+  console.log('newBlog: blogData: ', blogData)
+  
+  return {
+    id: 3   // 表示新建博客，插入到数据表里面的id（实际是一个自动生成的递增id）
+  }
+}
+
+const updateBlog = (id, blogData = {}) => {
+  // id 就是要更新博客的 id
+  // blogData 是一个博客对象，包含 title、content 属性
+  console.log('update Blog: ', `id: ${id}`, blogData)
+
+  // 返回true表示更新成功
+  return true
+}
+
 module.exports = {
   getList,
-  getDetail
+  getDetail,
+  newBlog,
+  updateBlog
 }
--- blog-Frameless/src/router/blog.js
+++ blog-Frameless/src/router/blog.js
@@ -1,14 +1,17 @@
 const {
   getList,
-  getDetail
+  getDetail,
+  newBlog,
+  updateBlog
 } = require('../controller/blog')
 
 const {
-  SuccessModel
+  SuccessModel, ErrorModel
 } = require('../model/resModel')
 
 const handleBlogRouter = (req, res) => {
   const method = req.method
+  const id = req.query.id
 
   // 获取博客列表
   if (method === 'GET' && req.path === '/api/blog/list') {
@@ -29,15 +32,17 @@ const handleBlogRouter = (req, res) => {
 
   // 新建博客
   if (method === 'POST' && req.path === '/api/blog/new') {
-    return {
-      msg: '这是新建博客的接口'
-    }
+    const data = newBlog(req.body)
+    return new SuccessModel(data)
   }
 
   // 更新博客
   if (method === 'POST' && req.path === '/api/blog/update') {
-    return {
-      msg: '这是更新博客的接口'
+    const result = updateBlog(id, req.body)
+    if (result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel('更新博客失败')
     }
   }
 
