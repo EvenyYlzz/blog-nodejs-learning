c1d6dd144414f70b712f9c74e1876f7f430bcb58
EvenyYlzz <172463074@qq.com>
2022-01-02 19:25:40 +0800

    feat: post跨域问题已解决，修饰登录界面

--- blog-frameless-front/src/views/Login.vue
+++ blog-frameless-front/src/views/Login.vue
@@ -1,13 +1,20 @@
 <template>
-  <div style="width:200px;">
-    <el-input v-model="username" placeholder="请输入账号" />
-    <el-input v-model="password" placeholder="请输入密码" />
+  <div>
+    <p>
+      <span>账号：</span>
+      <el-input style="width: 200px;" v-model="username" placeholder="请输入账号" />
+    </p>
+    <p>
+      <span>密码：</span>
+      <el-input style="width: 200px;" v-model="password" placeholder="请输入密码" />
+    </p>
     <el-button @click="login" type="primary">登录</el-button>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
+import { ElMessageBox, ElMessage } from 'element-plus'
 
 export default {
   name: 'Login',
@@ -21,6 +28,13 @@ export default {
   },
   methods: {
     login() {
+      if (!this.username || !this.password) {
+        ElMessage({
+          message: '请将用户名或密码输入完成后再点击登录!',
+          type: 'warning',
+        })
+        return
+      }
       axios.post('http://localhost:8000/api/user/login', {
         username: this.username,
         password: this.password,
@@ -31,7 +45,13 @@ export default {
       }).then(json => {
         return json.data
       }).then(res => {
-        console.log('res', res)
+        this.loginResult(res)
+      })
+    },
+    loginResult(resObj) {
+      ElMessageBox.alert(`登录${resObj.errno === -1 ? '失败' : '成功'}`, '登录结果', {
+        confirmButtonText: '确定',
+        type: resObj.errno === -1 ? 'error' : 'success',
       })
     }
   }
