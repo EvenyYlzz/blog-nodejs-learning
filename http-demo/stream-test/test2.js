05f031f40e2730fd420c117749a67f4041b3207d
EvenyYlzz <172463074@qq.com>
2022-03-01 23:25:36 +0800

    feat: 流的具体例子demo

---
+++ http-demo/stream-test/test2.js
@@ -0,0 +1,16 @@
+// 文件IO与http网络结合
+
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const fileName1 = path.resolve(__dirname, 'data.txt')
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const readStream = fs.createReadStream(fileName1)
+    readStream.pipe(res)
+  }
+})
+
+server.listen(8000)
